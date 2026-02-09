8d20795f7a26d51424374df35bf011996fb90aca
Sudarshan <godlsudarsan@gmail.com>
2026-02-09 18:24:29 +0530

    plugin registry file to maintain the tasks

---
+++ cooper/work/plugin_registry.py
@@ -0,0 +1,9 @@
+class PluginRegistry:
+    def __init__(self):
+        self.plugins = []
+
+    def register(self, plugin):
+        self.plugins.append(plugin)
+
+    def get_plugins_for_intent(self, intent):
+        return [p for p in self.plugins if p.can_handle(intent)]
