f63a78a62689505270bd28f2a6a9b1d8d05c346c
Sudarshan <godlsudarsan@gmail.com>
2026-02-10 21:47:43 +0530

    modified intent_router file for plugins

--- cooper/intent_router.py
+++ cooper/intent_router.py
@@ -2,8 +2,9 @@ from cooper.intent_rules import rule_intent
 
 def get_intent(command: str):
     intent = rule_intent(command)
-
     if intent:
         return intent
-
     return {"action": "unknown", "target": None}
+
+def detect_intent(command: str):
+    return get_intent(command)
