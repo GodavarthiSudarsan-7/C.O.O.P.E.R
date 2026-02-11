94859d3a1c9bcdd1668c06b2785bb52635717e49
Sudarshan <godlsudarsan@gmail.com>
2026-02-12 00:06:47 +0530

    had few changes in main .py file for the process

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
 from cooper.shell import run_shell
-
+from cooper.work.plugin_registry import registry
+from cooper.work.planner_plugin import PlannerPlugin
+from cooper.work.executor_plugin import ExecutorPlugin
+from cooper.work.critic_plugin import CriticPlugin
+from cooper.work.responder_plugin import ResponderPlugin
 
 def main():
+    registry.register(PlannerPlugin())
+    registry.register(CriticPlugin())
+    registry.register(ExecutorPlugin())
+    registry.register(ResponderPlugin())
     run_shell()
-
 
 if __name__ == "__main__":
     main()
